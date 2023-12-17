54a59fc78aca366212c1f30e7c9d01cff18d6789
Vishless <mail.vishnupk@gmail.com>
2023-12-17 22:35:05 +0530

    Replace gruvbox with gruvbox-material

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,6 +1,7 @@
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
+
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system {
@@ -22,7 +23,7 @@ local plugins = {
   'tpope/vim-rhubarb',
 
   -- Theme
-  { 'morhetz/gruvbox'},
+  { 'sainnhe/gruvbox-material'},
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
