04bcb6722548ed9c50c124b6df72dad4b5b81351
Vishless <mail.vishnupk@gmail.com>
2023-12-17 22:36:46 +0530

    Remove default binds in telescope

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,19 @@
+
+-- See `:help telescope` and `:help telescope.setup()`
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+
+-- Enable telescope fzf native, if installed
+pcall(require('telescope').load_extension, 'fzf')
+
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
