2923c9ca0b6f8caa11898217eb2e0dce42e2a65a
Vishless <mail.vishnupk@gmail.com>
2023-12-18 10:45:00 +0530

    Setup global variable vim in neodev

--- after/plugin/neodev.lua
+++ after/plugin/neodev.lua
@@ -1,2 +1,19 @@
--- Setup neovim lua configuration
 require('neodev').setup()
+
+-- then setup your lsp server as usual
+local lspconfig = require('lspconfig')
+
+-- example to setup lua_ls and enable call snippets
+lspconfig.lua_ls.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      completion = {
+        callSnippet = "Replace"
+      }
+    }
+  }
+})
