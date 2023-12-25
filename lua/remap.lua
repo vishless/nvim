c52680bc728d1cabb877d834c7731aaa5e3a0fea
Vishnu Prasanth <mail.vishnupk@gmail.com>
2023-12-25 22:37:43 +0530

    Update netrw keymaps

--- lua/remap.lua
+++ lua/remap.lua
@@ -4,10 +4,11 @@ vim.g.maplocalleader = ' '
 
 vim.g.netrw_banner = 0
 vim.g.netrw_keepdir = 0
+vim.g.netrw_localrmdir='rm -rf'
 
-vim.keymap.set("n", "<leader>nf", vim.cmd.Ex)
-vim.keymap.set("n", "<leader>ns", vim.cmd.Sex)
-vim.keymap.set("n", "<leader>nv", vim.cmd.Vex)
+vim.keymap.set("n", "<leader>nf", vim.cmd.Explore)
+vim.keymap.set("n", "<leader>ns", vim.cmd.Sexplore)
+vim.keymap.set("n", "<leader>nv", vim.cmd.Vexplore)
 
 -- TODO: Fix this
 --vim.g.netrw_list_hide = "\(^\|\s\s\)\zs\.\S\+"
