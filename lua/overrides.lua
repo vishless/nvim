819aea59fb48443566480f98f7a10a6841719df0
Vishnu Prasanth <mail.vishnupk@gmail.com>
2024-10-06 11:31:12 +0530

    Create overrides.lua

---
+++ lua/overrides.lua
@@ -0,0 +1,13 @@
+
+-- Mainly to suppress the below treesitter warning:
+-- vim.treesitter.get_parser will return nil instead of raising an error in Neovim 0.12
+
+local function safe_get_parser(bufnr, lang)
+    local ok, parser = pcall(vim.treesitter.get_parser, bufnr, lang)
+    if ok then
+        return parser
+    end
+    return nil
+end
+
+vim.treesitter.get_parser = safe_get_parser
