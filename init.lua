d2194232587318c0d72d8e0af44c90a21f2b6f52
Vishnu Prasanth <mail.vishnupk@gmail.com>
2025-11-05 09:57:23 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -7,6 +7,8 @@ vim.o.signcolumn = "yes"
 vim.o.expandtab = true
 vim.o.wrap = false
 vim.o.winborder = "rounded"
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 vim.g.mapleader = " "
 vim.keymap.set('n', '<leader>o', ':update<CR> :source<CR>')
@@ -14,11 +16,32 @@ vim.keymap.set('n', '<leader>w', ':write<CR>')
 vim.keymap.set('n', '<leader>q', ':quit<CR>')
 
 vim.pack.add({
-    { src = "https://github.com/vague-theme/vague.nvim" },
-    { src = "https://github.com/stevearc/oil.nvim" },
     { src = "https://github.com/nvim-mini/mini.pick" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/stevearc/oil.nvim" },
+    { src = "https://github.com/tpope/vim-fugitive" },
+    { src = "https://github.com/catppuccin/nvim" },
 })
+
+require('mini.pick').setup()
+local pick = require('mini.pick')
+vim.keymap.set('n', '<leader>pf', pick.builtin.files, { desc = '[P]roject [F]iles' })
+vim.keymap.set('n', '<leader>ps', pick.builtin.grep, { desc = '[P]roject [S]earch' })
+vim.keymap.set('n', '<leader>pb', pick.builtin.buffers, { desc = '[P]roject [B]uffers' })
+vim.keymap.set('n', '<leader>pw', function()
+  pick.builtin.grep({ pattern = vim.fn.expand('<cword>') })
+end, { desc = '[P]roject [W]ord under cursor' })
+vim.keymap.set('n', '<leader>pg', function()
+  if vim.fn.system('git rev-parse --is-inside-work-tree') ~= 'true\n' then
+    vim.notify('Not a git repository — showing all files instead', vim.log.levels.WARN)
+    return pick.builtin.files()
+  end
+
+  pick.start({
+    source = { items = vim.fn.systemlist('git ls-files') },
+    options = { prompt = 'Git files' },
+  })
+end, { desc = '[P]roject [G]it files' })
 
 vim.lsp.enable({ "lua_ls", "solargraph" })
 vim.keymap.set('n', '<leader>lf', vim.lsp.buf.format)
@@ -35,18 +58,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 vim.cmd("set completeopt+=noselect")
 
-require "mini.pick".setup()
-vim.keymap.set('n', '<leader>f', ':Pick files<CR>')
-
 require "oil".setup()
 vim.keymap.set('n', '-', ':Oil<CR>', {desc = 'Open parent directory'})
 
-require("vague").setup({
-    transparent = true,
-    style = {
-        strings = "none",
-    }
-})
+vim.keymap.set('n', '<leader>gs', ':Git<CR>', { desc = '[G]it [S]tatus' })
 
-vim.cmd("colorscheme vague")
+vim.cmd("colorscheme catppuccin")
 vim.cmd(":hi statusline guibg=NONE")
